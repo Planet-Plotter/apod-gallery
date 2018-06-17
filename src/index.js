2c5e4b0aa8985c43c89080e91be300dd2a60fc0e
SethDonohue <donohue.seth@gmail.com>
2018-06-17 12:09:27 -0700

    added README, moved files, changes page title

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ApodGallery from './apod-gallery/components/apod-gallery';
+import ApodGallery from './components/apod-gallery';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<ApodGallery />, document.getElementById('apod-gallery'));
